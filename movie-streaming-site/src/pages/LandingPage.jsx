47817896a3e867e6cd726def386ef08a2c2ab13a
Yisehak Derje <isaacdereje50@gmail.com>
2026-07-29 16:55:28 +0300

    separate the reason into its own file in data folder

--- movie-streaming-site/src/pages/LandingPage.jsx
+++ movie-streaming-site/src/pages/LandingPage.jsx
@@ -1,30 +1,9 @@
 import { useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useHomeMovies } from '../hooks/useHomeMovies'
+import { reasons } from '../data/reasons'
 import './LandingPage.css'
 
-const reasons = [
-  {
-    title: 'Enjoy on your TV',
-    body: 'Watch on Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.',
-    icon: 'tv',
-  },
-  {
-    title: 'Download your shows to watch offline',
-    body: 'Save your favorites easily and always have something to watch.',
-    icon: 'download',
-  },
-  {
-    title: 'Watch everywhere',
-    body: 'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.',
-    icon: 'spotlight',
-  },
-  {
-    title: 'Create profiles for kids',
-    body: 'Send kids on adventures with their favorite characters in a space made just for them.',
-    icon: 'kids',
-  },
-]
 
 const initialSignupData = {
   name: '',
